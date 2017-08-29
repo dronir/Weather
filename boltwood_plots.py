084b15b469498a5e881d38d811495ebaafdbb19e
Olli Wilkman <olli.wilkman@iki.fi>
2017-08-29 13:33:52 +0300

    Commit some old changes

--- boltwood_plots.py
+++ boltwood_plots.py
@@ -1,6 +1,9 @@
 import seaborn as sns
+import pandas as pd
 import matplotlib.pyplot as plt
-from pandas_reader import read_boltwood
+import matplotlib as mpl
+from pandas_reader import read_boltwood, read_clouds
+from plot_tools import fractional_bar_plot
 from sys import argv
 
 def plot_skytemp_clouds(data):
@@ -17,12 +20,35 @@ def plot_hour_dark_clouds(data):
     sns.countplot(x="hour", data=selection, color="black")
 
 def plot_month_dark_clouds(data):
-    selection = data.loc[(data["daylight"]=="dark") & (data["clouds"]=="clear")]
-    sns.countplot(x="month", data=selection, color="black")
+    selection = data.loc[(data["daylight"] == "dark")]
+    fractional_bar_plot(x="month", hue="clouds", data=selection, palette="Blues_r",
+                        labelsH=["clear", "cloudy", "very cloudy", "unknown"])
+
+def plot_month_humidity(data):
+    selection = data.loc[(data["clouds"]!="unknown")]
+    sns.boxplot(x="month", y="humidity", hue="clouds", data=selection)
+
+def plot_month_clouds_grid(data):
+    selection = data#.loc[(data["clouds"]!="unknown")]
+    sns.countplot(x="month", hue="clouds", data=selection)
+
+
+def compare_Nuuksio_clouds(data, cloud_file):
+    FMI_data = read_clouds(cloud_file).sort_index()
+    start,end = FMI_data.index[0], FMI_data.index[-1]
+    selection = data.loc[data["skyTemp"]>-200].reindex(FMI_data.index, method="pad")
+    X = selection["skyTemp"]
+    Y = FMI_data["cloudCover"]
+    data = pd.concat([X, Y], axis=1)
+    sns.boxplot(x="cloudCover", y="skyTemp", data=data)
+
+    
+
 
 if __name__=="__main__":
     print("Reading data...")
-    data = read_boltwood(argv[1])
+    data = read_boltwood(argv[1]).sort_index()
+    print(data.index[0], data.index[-1])
     
     print("Making plot...")
     plot_month_dark_clouds(data)
--- pandas_reader.py
+++ pandas_reader.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from datetime import datetime
+from numpy import nan as NaN
 from sys import argv
+from os import path
+
 
 BOLTWOOD_COLUMNS = [
     "day", "month", "year", "hour", "minute", "second",
@@ -12,7 +16,6 @@ BOLTWOOD_COLUMNS = [
     "heaterPower",
     "windUnits",
 ]
-
 MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 BOLTWOOD_CLOUD_VALUE = ["unknown", "clear", "cloudy", "very cloudy"]
@@ -26,15 +29,61 @@ BOLTWOOD_CONVERTERS = {
     "rain" : lambda i: BOLTWOOD_RAIN_VALUE[int(i)],
     "wind" : lambda i: BOLTWOOD_WIND_VALUE[int(i)],
     "daylight" : lambda i: BOLTWOOD_DAYLIGHT_VALUE[int(i)],
-    "month" : lambda i: MONTHS[int(i)-1]
+    "month" : lambda i: MONTHS[int(i)-1],
+    "year" : int,
+    "day" : int,
+    "hour" : int,
+    "minute" : int,
+    "second" : int
+}
+
+NUUKSIO_CONVERTERS = {
+    "cloudCover" : float
+}
+
+VAISALA_CONVERTERS = {
+    "data" : lambda s: NaN if "/" in s else float(s)
 }
 
 def read_boltwood(filename):
     """Read Boltwood data file and return a Pandas DataFrame.
     The column names defined above are used, and the indexing is by the timestamp."""
+    data = pd.read_table(filename, delim_whitespace=True, index_col=0,
+                         parse_dates={"timestamp":[0,1,2,3,4,5]}, keep_date_col=True,
+                         names=BOLTWOOD_COLUMNS, converters=BOLTWOOD_CONVERTERS,
+                         infer_datetime_format=True)
+    # This is slow and crude, but can't of a better way to do it now:
+    dates = [datetime.strptime(x, "%d %b %Y %H %M %S") for x in data.index]
+    return data.set_index(pd.DatetimeIndex(dates))
+
+
+def read_clouds(filename):
     return pd.read_table(filename, delim_whitespace=True, index_col=0,
-                         parse_dates={"timestamp":[2,1,0,3,4,5]}, keep_date_col=True,
-                         names=BOLTWOOD_COLUMNS, converters=BOLTWOOD_CONVERTERS)
+                         parse_dates={"timestamp":[0]}, names=["foo", "cloudCover"],
+                         converters=NUUKSIO_CONVERTERS)
+
+
+def convert_datetime(dates, times):
+    N = len(dates)
+    output = []
+    for i in range(N):
+        line = "{} {}".format(dates[i], times[i])
+        output.append(pd.to_datetime(line, format="%d.%m.%Y %H:%M:%S"))
+    return output
+
+form = "%d.%m.%Y %H:%M:%S"
+
+def read_vaisala(filename):
+    data = pd.read_table(filename, delim_whitespace=True, names=["date", "time", "value"],
+                         encoding="utf-8-sig", na_values="///", parse_dates=[[0,1]],
+                         index_col=0, converters=VAISALA_CONVERTERS,
+                         date_parser=convert_datetime)
+    return data
+
+def pickle_vaisala(filename):
+    data = read_vaisala(filename)
+    data.to_pickle("pickled_vaisala.dat")
 
 if __name__=="__main__":
-    print(read_boltwood(argv[1]))
+    data = read_boltwood(argv[1])
+    data.to_pickle("pickled_{}.dat".format(path.basename(argv[1])))
